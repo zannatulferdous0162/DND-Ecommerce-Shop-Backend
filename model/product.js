471f545333184972f56afe713ef69393d71b5c98
zannatulferdous0162 <zannatulferdous0162@gmail.com>
2024-08-18 19:59:05 +0600

    10nd commit

--- model/product.js
+++ model/product.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
-const productSchema = mongoose.Schema({
-    name: { type: String, required: true },
-    image: { type: String},
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    category: { type: String, required: true },
-    ratings: { type: Number, default: 0 },
-    createdAt: { type: Date, default: Date.now },
-})
+// const productSchema = mongoose.Schema({
+//     name: { type: String, required: true },
+//     image: { type: String},
+//     description: { type: String, required: true },
+//     price: { type: Number, required: true },
+//     category: { type: String, required: true },
+//     ratings: { type: Number, default: 0 },
+//     createdAt: { type: Date, default: Date.now },
+// })
 
 const product= mongoose.model('product',productSchema)
 
